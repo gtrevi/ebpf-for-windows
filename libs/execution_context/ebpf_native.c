4229b73c2aeb9ad0472f51dd61fce7fe3024be31
Gianni Trevisiol <gtrevi@microsoft.com>
2022-12-08 12:03:57 -0800

    wip

--- libs/execution_context/ebpf_native.c
+++ libs/execution_context/ebpf_native.c
@@ -203,7 +203,8 @@ ebpf_native_acquire_reference(_Inout_ ebpf_native_module_t* module)
     ebpf_assert(module->base.marker == _ebpf_native_marker);
 
     int32_t new_ref_count = ebpf_interlocked_increment_int32(&module->base.reference_count);
-    ebpf_assert(UNREFERENCED_PARAMETER(new_ref_count) != 1);
+    UNREFERENCED_PARAMETER(new_ref_count);
+    ebpf_assert(new_ref_count != 1);
 }
 
 void
