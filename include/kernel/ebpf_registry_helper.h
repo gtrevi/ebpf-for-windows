8ac18b8378cda0568f58e17a4bbb65adb7e25f9b
Gianni Trevisiol <giovanni.trevisiol@gmail.com>
2023-02-19 16:22:28 -0800

    wip

--- include/kernel/ebpf_registry_helper.h
+++ include/kernel/ebpf_registry_helper.h
@@ -13,18 +13,12 @@
 #define GUID_STRING_LENGTH 38 // not including the null terminator.
 
 typedef _Return_type_success_(NT_SUCCESS(return )) uint32_t ebpf_registry_result_t;
-
 typedef HANDLE ebpf_registry_key_t;
-
-_Success_(return == 0) NTSTATUS
-    convert_guid_to_string(_In_ const GUID* guid, _Out_writes_all_(string_size) wchar_t* string, size_t string_size);
-
-void
-close_registry_key(ebpf_registry_key_t key);
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Mocks for building platform_kernel & netebpfext when including the common ebpf_store_helper.h in kernel mode.
-// Currently these prototypes only need implementations in user mode, as they are not referenced in kernel mode.
+// Currently these prototypes only need (existing) implementations in user mode, as they are not referenced in kernel
+// binaries.
 _Success_(return == 0) uint32_t open_registry_key(
     ebpf_registry_key_t root_key, _In_opt_z_ const wchar_t* sub_key, uint32_t flags, _Out_ ebpf_registry_key_t* key);
 _Must_inspect_result_ ebpf_registry_result_t
@@ -32,6 +26,12 @@ delete_registry_key(ebpf_registry_key_t root_key, _In_z_ const wchar_t* sub_key)
 _Must_inspect_result_ ebpf_registry_result_t
 delete_registry_tree(ebpf_registry_key_t root_key, _In_opt_z_ const wchar_t* sub_key);
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+_Success_(return == 0) NTSTATUS
+    convert_guid_to_string(_In_ const GUID* guid, _Out_writes_all_(string_size) wchar_t* string, size_t string_size);
+
+void
+close_registry_key(ebpf_registry_key_t key);
 
 _Must_inspect_result_ ebpf_registry_result_t
 write_registry_value_binary(
