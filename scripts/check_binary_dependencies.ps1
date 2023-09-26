d882e8ad0dfbf483eb86102ebc34bd153c588feb
Gianni Trevisiol <gtrevi@users.noreply.github.com>
2023-09-25 19:00:53 -0700

    wip

--- scripts/check_binary_dependencies.ps1
+++ scripts/check_binary_dependencies.ps1
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: MIT
 
 param ([parameter(Mandatory=$false)][string] $BuildArtifact,
-       [parameter(Mandatory=$false)][string] $BuildConfiguration,
        [Parameter(Mandatory=$True)] [string] $LogFileName)
 
 
@@ -18,6 +17,8 @@ function Test-CppBinaryDependencies {
         [string]$TextFilePath
     )
 
+    Write-Log "Checking binary dependencies for [$BuildArtifact - $FilePath] against [$TextFilePath]..." -ForegroundColor Green
+
     # Run link.exe to extract dependencies
     $Output = & "dumpbin.exe" /dependents $FilePath | Out-String
 
@@ -28,30 +29,29 @@ function Test-CppBinaryDependencies {
     $Dependencies = $Output -split "`n" | Where-Object { $_.Trim().EndsWith(".dll") } | ForEach-Object { $_.Trim() }
     $Dependencies = $Dependencies[1..$Dependencies.Length] # Discard the first line, which always contains the dumped file itself.
 
-    # Compare dependencies with expected binaries
+    # Compare dependencies with the expected binaries
     $MissingBinaries = Compare-Object -ReferenceObject $Dependencies -DifferenceObject $ExpectedBinaries -PassThru
     $ExtraBinaries = Compare-Object -ReferenceObject $ExpectedBinaries -DifferenceObject $Dependencies -PassThru
-
     if ($MissingBinaries -or $ExtraBinaries) {
         Write-Log "Mismatch found between dependencies in the file and the list:" -ForegroundColor Red
         Write-Log "Missing Dependencies:" -ForegroundColor Red
         Write-Log $MissingBinaries
         Write-Log "Extra Dependencies:" -ForegroundColor Red
         Write-Log $ExtraBinaries
-        throw "Dependency check failed."
+        throw "Dependency checks failed."
     } else {
-        Write-Log "All dependencies match the list." -ForegroundColor Green
+        Write-Log "All dependencies match the expected list." -ForegroundColor Green
     }
 }
 
-if ($BuildArtifact -eq "Build-x64" && $BuildConfiguration -eq "Release") {
+if ($BuildArtifact -eq "Build-x64") {
     Test-CppBinaryDependencies -FilePath "bpftool.exe" -TextFilePath "$WorkingDirectory\..\..\scripts\check_binary_dependencies_bpftool_exe_regular.txt" | Out-Null
-    Test-CppBinaryDependencies -FilePath "ebpfsvc.exe" -TextFilePath "$WorkingDirectory\..\..\scripts\check_binary_dependencies_ebpfsvc_exe_regular.txt" | Out-Null
     Test-CppBinaryDependencies -FilePath "ebpfapi.dll" -TextFilePath "$WorkingDirectory\..\..\scripts\check_binary_dependencies_ebpfapi_dll_regular.txt" | Out-Null
     Test-CppBinaryDependencies -FilePath "ebpfnetsh.dll" -TextFilePath "$WorkingDirectory\..\..\scripts\check_binary_dependencies_ebpfnetsh_dll_regular.txt" | Out-Null
+    Test-CppBinaryDependencies -FilePath "ebpfsvc.exe" -TextFilePath "$WorkingDirectory\..\..\scripts\check_binary_dependencies_ebpfsvc_exe_regular.txt" | Out-Null
 }
 
-if ($BuildArtifact -eq "Build-x64-native-only" && $BuildConfiguration -eq "Release") {
+if ($BuildArtifact -eq "Build-x64-native-only") {
     Test-CppBinaryDependencies -FilePath "bpftool.exe" -TextFilePath "$WorkingDirectory\..\..\scripts\check_binary_dependencies_bpftool_exe_nativeonly.txt" | Out-Null
     Test-CppBinaryDependencies -FilePath "ebpfapi.dll" -TextFilePath "$WorkingDirectory\..\..\scripts\check_binary_dependencies_ebpfapi_dll_nativeonly.txt" | Out-Null
     Test-CppBinaryDependencies -FilePath "ebpfnetsh.dll" -TextFilePath "$WorkingDirectory\..\..\scripts\check_binary_dependencies_ebpfnetsh_dll_nativeonly.txt" | Out-Null
