d7c56f4cad10b03377cdf0b7f9dad2ba7c3caa2c
Gianni Trevisiol <gtrevi@users.noreply.github.com>
2023-10-04 18:03:33 -0700

    wip

--- scripts/check_msi_installation.ps1
+++ scripts/check_msi_installation.ps1
@@ -8,16 +8,56 @@ param (
 
 Push-Location $WorkingDirectory
 
+$InstallPath = "$env:ProgramFiles\ebpf-for-windows";
 
 # Define the test cases
-$testCases = @{
-    "Build-x64_Debug" = @{
-        "unittests.exe" = "..\..\scripts\check_binary_dependencies_bpftool_exe_regular_debug.txt"
-    }
-    "Build-x64-native-only_NativeOnlyRelease" = @{
-        "bpftool.exe" = "..\..\scripts\check_binary_dependencies_bpftool_exe_nativeonly_release.txt"
-    }
+$expectedFileLists = @{
+    "Build-x64_Debug" = "..\..\scripts\check_msi_installation_msi_files_debug.txt"
+    "Build-x64-native-only_NativeOnlyRelease" = "..\..\scripts\check_msi_installation_msi_files_nativeonly_release.txt"
 }
+
+function CompareFilesInDirectory {
+    param(
+        [string]$targetPath,
+        [string]$listFilePath
+    )
+
+    # Read the list of files from the file
+    $fileList = Get-Content $listFilePath
+
+    # Get all files and subdirectories in the target directory
+    $items = Get-ChildItem $targetPath -Recurse
+
+    # Initialize a boolean variable to track whether all files were found
+    $allFilesFound = $true
+
+    # Initialize an array to store the found files
+    $foundFiles = @()
+
+    # Iterate through each item (file or directory) in the target path
+    foreach ($item in $items) {
+        if ($item.GetType() -eq [System.IO.FileInfo]) {
+            # If the item is a file, check if it's in the list
+            if ($fileList -contains $item.Name) {
+                $foundFiles += $item.Name
+            } else {
+                $allFilesFound = $false
+            }
+        }
+    }
+
+    # Display the found files
+    Write-Host "Found Files:"
+    $foundFiles
+
+    # Display the missing files
+    Write-Host "Missing Files:"
+    $missingFiles = Compare-Object $fileList $foundFiles -PassThru
+    $missingFiles
+
+    return $allFilesFound
+}
+
 
 function Install-MsiPackage {
     [CmdletBinding()]
@@ -45,18 +85,14 @@ function Install-MsiPackage {
     }
 }
 
+# Test the installation
+$allTestsPassed = $true
 try {
     Install-MsiPackage -MsiPath $MsiPath -MsiAdditionalArguments "$MsiAdditionalArguments"
+    $allTestsPassed =  CompareFilesInDirectory -targetPath $InstallPath -listFilePath $expectedFileLists[$BuildArtifact]
 } catch {
     Write-Host "Error: $_"
 }
-
-# Iterate over all the test cases
-$allTestsPassed = $true
-# foreach ($filePath in $testCases[$BuildArtifact].Keys) {
-#     $res = Install-MsiPackage -MsiPath $MsiPath -MsiAdditionalArguments  $testCases[$BuildArtifact][$filePath]
-#     $allTestsPassed = $allTestsPassed -and $res
-# }
 
 Pop-Location
 
